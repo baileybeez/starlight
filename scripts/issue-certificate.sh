d7d0a621d39b24641e02a3ba2fdafec4d6472962
BaileyBeez <thebaileybeez@gmail.com>
2025-02-26 15:18:16 -0500

    updated example/test scripts

--- scripts/issue-certificate.sh
+++ scripts/issue-certificate.sh
@@ -1,3 +1,3 @@
 # create certificates for testing on localhost
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
-    -keyout starlight.key -out starlight.crt -subj "/CN=localhost"
+    -keyout certs/starlight.key -out certs/starlight.crt -subj "/CN=localhost"
